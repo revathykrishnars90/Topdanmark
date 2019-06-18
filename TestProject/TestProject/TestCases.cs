6c4a0aa03d6e9617b5d55755beb7a9b5d658916b
pradeep.cr <pradeep.cr@quest-global.com>
2019-06-19 01:04:19 +0800

    Initial commit

---
+++ TestProject/TestProject/TestCases.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject;
+namespace TopDanmark
+{
+    [TestClass]
+    public class TestCases
+    {
+        [TestMethod]
+        public void TestCase1()
+        {
+            UnitTest1 test1 = new UnitTest1();
+            test1.TC_1_verifyTopDanmarkIsAppearingFirstInSearch();
+        }
+
+        [TestMethod]
+        public void Testcase2()
+        {
+            UnitTest1 test2 = new UnitTest1();
+            test2.TC_2_verifyAccessibleViaDifferentBrowsers();
+        }        
+    }
+}
---
+++ TestProject/TestProject/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+
+namespace TestProject
+{
+
+    public class UnitTest1
+    {
+        
+        public void TC_1_verifyTopDanmarkIsAppearingFirstInSearch()
+        {
+            ChromeDriver driver = new ChromeDriver();
+            INavigation navi =  driver.Navigate();
+            navi.GoToUrl("http://google.com");
+            IWebElement search = driver.FindElementByName("q");
+            search.SendKeys("topdanmark");
+            search.Click();
+            IWebElement searchButton = driver.FindElementByName("btnK");
+            searchButton.Click();
+            IWebElement resultsPanel = driver.FindElement(By.Id("search"));
+            ReadOnlyCollection<IWebElement> searchResults = resultsPanel.FindElements(By.XPath(".//a"));
+            Assert.AreEqual("Topdanmark.com - Topdanmark.com\r\nwww.topdanmark.com/en/", searchResults[0].Text);
+            driver.Close();
+        }
+
+        
+        public void TC_2_verifyAccessibleViaDifferentBrowsers()
+        {
+            verifyAccessibleVia(new InternetExplorerDriver());
+            verifyAccessibleVia(new ChromeDriver());
+            verifyAccessibleVia(new FirefoxDriver());
+        }
+
+        public void verifyAccessibleVia(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl("http://topdanmark.com");
+            driver.FindElement(By.ClassName("logo"));
+            driver.Close();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
